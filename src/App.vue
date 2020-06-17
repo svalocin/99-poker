8fce93079e04b4a15ca14b3d7f1a40dc89a59b6c
Slyerr <shiwei1211@gmail.com>
2020-06-17 12:47:56 +0800

    feat: 完成基础功能

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<style>
+html body #app {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
+
+
